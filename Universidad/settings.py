3a1fd403aabe5e7e197bde7c80c81393bcf984a6
David Rey <neisserrey@upeu.edu.pe>
2024-10-06 20:55:32 +0000

    DEPLOY REDNERsdfsdfsd

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nd11#!qlneem$mz%v#kg@-tp(kxe(-#u06wce(t23_eqwdxa80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
